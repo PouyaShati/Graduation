0872ffe1f069e02ef14bf5857bbf3a879a059314
Vampaneze <shati@ce.sharif.edu>
2018-02-02 20:52:02 +0330

    c

--- graduate/models.py
+++ graduate/models.py
@@ -1,31 +1,28 @@
 from django.db import models
 from django.utils import timezone
-from django.contrib.postgres.fields import ArrayField
-import enum
 
-# Create your models here.
-#class Question_Type(enum.Enum):
-#    Text = "TXT"
-#    Num_Int = "INT"
-#    Num_Real = "REAL"
-#    Doc = "DOC"
-#    Multiple = "CHOICE"
 
 
 
 class Question_Set(models.Model):
-    name = models.CharField(max_length=60)
+    name = models.CharField(max_length=60, null=True)
 
-    questions = ArrayField(models.CharField(max_length=1000), size=None)
-    question_types = ArrayField(models.CharField(max_length=20, choices=[
+class Answer_Set (models.Model):
+    name = models.CharField(max_length=60, null=True)
+
+class Question(models.Model):
+    text = models.CharField(max_length=1000)
+    type = models.CharField(max_length=20, choices=[
         ('Text','Text'),
         ('Integer','Integer'),
         ('Real','Real'),
         ('Document','Document'),
-        ('Multiple Choice','Multiple Choice'),]), size=None)
+        ('Multiple Choice','Multiple Choice'),])
+    belongs_to = models.ForeignKey(Question_Set, on_delete=models.CASCADE, null=True)
 
-class Answer_Set (models.Model):
-    answers = ArrayField(models.CharField(max_length=1000), size=None)
+class Answer(models.Model):
+    text = models.CharField(max_length=1000)
+    belongs_to = models.ForeignKey(Answer_Set, on_delete=models.CASCADE, null=True)
 
 
 
@@ -45,10 +42,10 @@ class Student_Task_Blueprint(Task_Blueprint):
     max_time = models.DateTimeField(default=timezone.ZERO)
 
 class Employee_Task_Blueprint(Task_Blueprint):
-    question_set = models.ForeignKey(Question_Set, on_delete=models.CASCADE)
+    question_set = models.ForeignKey(Question_Set, on_delete=models.CASCADE, null=True)
 
 class Form_Blueprint(Student_Task_Blueprint):
-    question_set = models.ForeignKey(Question_Set, on_delete=models.CASCADE)
+    question_set = models.ForeignKey(Question_Set, on_delete=models.CASCADE, null=True)
 
 class Payment_Blueprint(Student_Task_Blueprint):
     receiver = models.IntegerField(default=0)
@@ -99,27 +96,27 @@ class Process(models.Model):
     validated = models.BooleanField(default=False)
     # TODO add record
     # TODO implement functions
-    instance_of = models.ForeignKey(Process_Blueprint, on_delete=models.CASCADE)
-    owner = models.ForeignKey(Student, on_delete=models.CASCADE)
+    instance_of = models.ForeignKey(Process_Blueprint, on_delete=models.CASCADE, null=True)
+    owner = models.ForeignKey(Student, on_delete=models.CASCADE, null=True)
 
 class Task(models.Model):
     done = models.BooleanField(default=False)
     # TODO add record
     # TODO implement functions
-    process = models.ForeignKey(Process, on_delete=models.CASCADE)
+    process = models.ForeignKey(Process, on_delete=models.CASCADE, null=True)
 
 class Student_Task(Task):
     start_time = models.DateTimeField(default=timezone.ZERO)
 
 class Employee_Task(Task):
-    answer_set = models.OneToOneField(Answer_Set, on_delete=models.CASCADE)
+    answer_set = models.OneToOneField(Answer_Set, on_delete=models.CASCADE, null=True)
     # TODO implement functions
-    instance_of = models.ForeignKey(Employee_Task_Blueprint, on_delete=models.CASCADE)
+    instance_of = models.ForeignKey(Employee_Task_Blueprint, on_delete=models.CASCADE, null=True)
 
 class Form(Student_Task):
-    answer_set = models.OneToOneField(Answer_Set, on_delete=models.CASCADE)
-    instance_of = models.ForeignKey(Form_Blueprint, on_delete=models.CASCADE)
+    answer_set = models.OneToOneField(Answer_Set, on_delete=models.CASCADE, null=True)
+    instance_of = models.ForeignKey(Form_Blueprint, on_delete=models.CASCADE, null=True)
 
 class Payment(Student_Task):
     amount = models.IntegerField(default=0)
-    instance_of = models.ForeignKey(Payment_Blueprint, on_delete=models.CASCADE)
+    instance_of = models.ForeignKey(Payment_Blueprint, on_delete=models.CASCADE, null=True)
--- graduate/urls.py
+++ graduate/urls.py
@@ -3,5 +3,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.index, name='index'),
+    path('studentDetail/<int:question_id>/', views.student_detail, name='student detail'),
+    path('processBlueprint/<int:question_id>/', views.process_blueprint_detail, name='process blueprint detail'),
 ]
