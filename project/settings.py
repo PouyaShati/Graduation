57de8bc5ae4291ada06b1de71e4b2bf73c470743
aarash <aarashfeyzi@yahoo.com>
2018-02-06 20:31:31 +0330

    fix settings

--- project/settings.py
+++ project/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'bootstrapform',
     'graduate.apps.GraduateConfig',
     'Student.apps.StudentConfig',
     'MyUser.apps.MyuserConfig',
